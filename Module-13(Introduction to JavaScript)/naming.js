5d1b0bde2400a918c0d0e553bc2a0aa437d13877
ashraful-hoda <asrafulhoda789@gmail.com>
2026-01-23 00:27:04 +0600

    naming a variable

---
+++ Module-13(Introduction to JavaScript)/naming.js
@@ -0,0 +1,21 @@
+// 1. no keyword in variable name
+// var false = 45;
+
+// 2. no space or gap in variable name
+// var is happy = false;
+
+// 3. no qoute
+// var "address" = "CTG";
+
+// 4. can not start with a number but number other than the first letter is allowed
+
+// 5. name is case sensetive
+
+address
+Address
+Address
+
+// 6. how to write a long variable name
+var my_current_home_address; // snake case
+var myCurrentHomeAddress; // camel case
+var MyCurrentHomeAddress; // Pascal case
