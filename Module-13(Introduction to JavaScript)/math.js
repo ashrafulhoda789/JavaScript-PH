65a59affaad5f9349e39a69c48d5417166cae977
ashraful-hoda <asrafulhoda789@gmail.com>
2026-01-22 23:59:56 +0600

    introduction of javascript

---
+++ Module-13(Introduction to JavaScript)/math.js
@@ -0,0 +1,16 @@
+/* 
+    *******************************************************************
+    *               MATH NEEDED FOR PROGRAMMING
+    * *****************************************************************
+    * 1. Number: +ve, -ver, 0 [50, -9, 0]
+    * 2. Integer: 2, 88, [odd, even] : 2, 4, 897
+    * 3. FLoat: 5.6
+    * 4. Operation: +, -, *, /
+    * 5. More operations: %
+    * 6. Percentage: 5%, 
+    * 7. Sum: 14, 18, 16
+    * 8. Average: 4, 18, 16
+    * 9. Max/Min: 
+    * 10. Series:
+    * 11. Area: Triangle, Rectangle, Square
+*/
