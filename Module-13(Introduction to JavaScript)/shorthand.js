c7d37ee0bef32c26835f709a558d695ce9ecf9dc
ashraful-hoda <asrafulhoda789@gmail.com>
2026-01-23 00:49:19 +0600

    arithmetic operation

---
+++ Module-13(Introduction to JavaScript)/shorthand.js
@@ -0,0 +1,12 @@
+var price = 35;
+// price = price + 10;
+price += 10;
+console.log(price);
+
+var fridge = 35000;
+fridge -= 1000; // fridge = fridge - 10;
+console.log(fridge);
+
+var orangePrice = 10;
+orangePrice *= 3; // orangePrice = orangePrice * 3;
+console.log(orangePrice);
