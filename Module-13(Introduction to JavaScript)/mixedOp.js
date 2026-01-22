371e8ae647640343ea72a2ab693910a09c9d3b02
ashraful-hoda <asrafulhoda789@gmail.com>
2026-01-23 00:52:55 +0600

    last topic mixed operation

---
+++ Module-13(Introduction to JavaScript)/mixedOp.js
@@ -0,0 +1,9 @@
+var first = 'Ashraful';
+var second = 'Hoda';
+
+var fullName = first + second;
+// console.log(fullName);
+// console.log('40' + 2);
+
+console.log(40 - '2');
+console.log('50' - 10);
