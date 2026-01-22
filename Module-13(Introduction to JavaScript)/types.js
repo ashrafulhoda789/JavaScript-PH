8c0c649af87aeb2406d21d55b89eaf1857ed7c35
ashraful-hoda <asrafulhoda789@gmail.com>
2026-01-23 00:10:58 +0600

    types of Js

---
+++ Module-13(Introduction to JavaScript)/types.js
@@ -0,0 +1,20 @@
+// String
+var name = "Ashraful Hoda Jamshed";
+// console.log(name);
+
+var address = "Majar gate, Chawkbazar";
+var country = "Bangladesh";
+
+// Boolean
+var isRich = true;
+console.log(isRich);
+
+var passed = false;
+console.log(passed)
+
+console.log(typeof passed)
+console.log(typeof address)
+
+// Number
+var price = 550;
+console.log(typeof price);
