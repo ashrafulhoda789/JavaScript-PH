986a094a4e2d6bd7f6c56af00258045c81652195
ashraful-hoda <asrafulhoda789@gmail.com>
2026-01-23 23:37:52 +0600

    and or operator

---
+++ Module-14(Fundamental Concept of Condition)/multiple.js
@@ -0,0 +1,17 @@
+const salary = 21001;
+const isBCS = true;
+const height = 78;
+
+// if (salary > 20000 && height > 61){
+//     console.log("Better Choice");
+// }
+// else{
+//     console.log("Not better choice");
+// }
+
+if(salary > 25000 || height > 72 || isBCS == true){
+    console.log("Better choice");
+}
+else{
+    console.log("Not Better choice");
+}
