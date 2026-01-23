0b019e6ccc47b1089337b3ff5ca4f4917e9963a1
ashraful-hoda <asrafulhoda789@gmail.com>
2026-01-24 00:14:31 +0600

    shortcut

---
+++ Module-14(Fundamental Concept of Condition)/shortcut.js
@@ -0,0 +1,16 @@
+const isLeader = true;
+
+// if(isLeader){
+//     console.log("Leader is coming");
+// }
+// else{
+//     console.log("Not coming");
+// }
+
+const isPassed = false;
+if(!isPassed){
+    console.log("Yes");
+}
+else{
+    console.log("No");
+}
