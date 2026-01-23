5312a5c05189593784f78202a8b85607c1d75a2c
ashraful-hoda <asrafulhoda789@gmail.com>
2026-01-23 23:29:29 +0600

    if condition

---
+++ Module-14(Fundamental Concept of Condition)/if.js
@@ -0,0 +1,28 @@
+/**
+ * -------------STRUCTURE-----------------
+ * if (condition) {
+ *         execute code it the condition above is true
+ * }
+ * else{
+ *         execute code it the condition above is false
+ * }
+*/
+
+if(13 < 10){
+    console.log("I have a smaller value");
+}
+
+var biriyaniPrice = 250;
+
+if(biriyaniPrice < 300){
+    console.log("Give me some biriyani");
+}
+
+var weight = 20;
+
+if (weight <= 20){
+    console.log("I will carry myself");
+}
+else{
+    console.log("I will rent a rickshaw");
+}
