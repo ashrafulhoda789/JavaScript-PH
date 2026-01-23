999082cb30737eb3ada60dc909f927c297f9d572
ashraful-hoda <asrafulhoda789@gmail.com>
2026-01-23 23:21:12 +0600

    comparison

---
+++ Module-14(Fundamental Concept of Condition)/comparison.js
@@ -0,0 +1,27 @@
+// console.log(5 > 10);
+// console.log(5 < 10);
+// console.log(5 == 10);
+// console.log(10 == 10);
+// console.log(15 >= 10);
+// console.log(5 >= 10);
+// console.log(5 <= 10);
+// console.log(10 <= 10);
+
+// -------------------------------------
+
+var peyaraPrice = 40;
+var applePrice = 250;
+
+// console.log(peyaraPrice > applePrice);
+
+/**
+ * == ; Just compare value, don't check the data types
+ * === ; Compare value with data types
+*/
+
+// console.log(10 == '10'); // return true
+// console.log(10 === '10'); // Return false
+
+// --------------------------
+console.log(10 != '10');
+console.log(10 !== '10');
