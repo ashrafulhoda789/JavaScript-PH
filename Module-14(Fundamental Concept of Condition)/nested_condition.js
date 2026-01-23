a0584b82e357ff30eb111695a2a74aa2cc26b8c7
ashraful-hoda <asrafulhoda789@gmail.com>
2026-01-23 23:51:38 +0600

    nested condition

---
+++ Module-14(Fundamental Concept of Condition)/nested_condition.js
@@ -0,0 +1,18 @@
+const money = -10;
+
+if(money > 300){
+    console.log("Bro you are rich");
+}
+else{
+    if(money > 100){
+        console.log("You are nor rich nor poor");
+    }
+    else{
+        if(money > 0){
+            console.log("Eat a banana");
+        }
+        else{
+            console.log("You are a extra level poor");
+        }
+    }
+}
