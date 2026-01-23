f317ed092bdf26cda96c9c55754c52d678de93ac
ashraful-hoda <asrafulhoda789@gmail.com>
2026-01-23 23:06:07 +0600

    theory about compraison operator

---
+++ Module-14(Fundamental Concept of Condition)/compare_variable_and_comparison_operator.js
@@ -0,0 +1,49 @@
+/* 
+    * VARIABLE ==> container
+
+*/
+
+// Number
+var sunglassPrice = 80;
+
+// String
+var subject = "CSE";
+
+// Boolean
+var passed = true;
+
+/**
+ * NAMING CONVENTION
+ * 1. single word
+ * 2. no qoute
+ * 3. no gap or dash
+ * 4. can not start with number
+ * 5. can not be a keyword
+ * 6. prefer camelCase
+
+*/
+
+/**
+ * OPERATION
+ * 1. +, -, *, /, %
+ * 2. +=, -=. *=, /=
+ * 3. isNan
+ * 4. null
+ * 5. Number
+*/
+
+//-----------------------------------------------------------------
+
+/**
+ * COMPARISON
+ * 1. Bigger: >
+ * 2. less: <
+ * 3. equal: ==
+ * 4. greater than or equal: >=
+ * 5. less than or equal: <=
+ * 6. not equal: !=
+ * 
+ * -------------------------
+ * 7. and: &&
+ * 8. or: ||
+*/
