2a045fd38cc6b2c3b0d1d479d313048ddebe0000
ashraful-hoda <asrafulhoda789@gmail.com>
2026-01-24 22:30:31 +0600

    index of an array

---
+++ Module-14.5(Array-part-1)/index.js
@@ -0,0 +1,17 @@
+const numbers = [88, 77, 99, 421, 54, 63];
+//index          0   1   2   3    4   5
+
+console.log(numbers);
+
+// Get element value by index
+console.log(numbers[0]);
+
+const fourth = numbers[3];
+console.log(fourth);
+
+// set or update element value by index
+let abc = 45;
+abc = 99;
+
+numbers[1] = 56;
+console.log(numbers);
