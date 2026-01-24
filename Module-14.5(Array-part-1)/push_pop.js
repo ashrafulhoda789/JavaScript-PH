8811caffd35229930206f81b05a84d6876265b9b
ashraful-hoda <asrafulhoda789@gmail.com>
2026-01-24 22:48:22 +0600

    push pop shift and unshift operation

---
+++ Module-14.5(Array-part-1)/push_pop.js
@@ -0,0 +1,19 @@
+const ages = [];
+const numbers = [12, 87, 98, 45];
+
+console.log(numbers);
+numbers.push(44);
+numbers.push(64);
+numbers.push(33);
+numbers.push(498, 11, 9);
+
+console.log(numbers);
+
+
+const friends = ["Ami", "tmi", "sei"];
+friends.push("moi");
+console.log(friends);
+
+const last = friends.pop();
+console.log(friends);
+console.log(last);
---
+++ Module-14.5(Array-part-1)/shift_unshift.js
@@ -0,0 +1,7 @@
+const friends = ["Ami", "tmi", "sei"];
+console.log(friends);
+friends.shift() // Remove first value of the array
+
+console.log(friends);
+friends.unshift("Pepeya"); // Add a value in first to the array
+console.log(friends);
