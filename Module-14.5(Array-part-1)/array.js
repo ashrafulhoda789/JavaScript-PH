433a54fdde36c952dca73abb66cfbed9a1385319
ashraful-hoda <asrafulhoda789@gmail.com>
2026-01-24 21:49:59 +0600

    array declaration

---
+++ Module-14.5(Array-part-1)/array.js
@@ -0,0 +1,4 @@
+const numbers = [12, 22, 54, 65];
+const friends = ["Abul", "Babul", "Tabul"];
+const passed = [true, false, true, false, false];
+const mixedArray = [12, "Chawkbazar", true, 45.56];
