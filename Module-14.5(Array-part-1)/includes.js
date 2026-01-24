77218ce6fff594d4c1c37b4bd75757fbebea86a0
ashraful-hoda <asrafulhoda789@gmail.com>
2026-01-24 22:57:51 +0600

    includes isArray and indexOf method

---
+++ Module-14.5(Array-part-1)/includes.js
@@ -0,0 +1,11 @@
+const friends = ["Ami", "tmi", "sei"];
+
+console.log(friends.includes("tmi"));
+console.log(friends.includes("tei"));
+
+if(friends.includes("Ami")){
+    console.log("Party");
+}
+else{
+    console.log("No party");
+}
---
+++ Module-14.5(Array-part-1)/indexOf.js
@@ -0,0 +1,5 @@
+const friends = ["Ami", "tmi", "sei"];
+
+console.log(friends.indexOf("tmi"));
+
+console.log(friends.indexOf("toto"));
