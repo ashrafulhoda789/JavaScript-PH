16a375984d195e9358d584229d2002aa195507ae
ashraful-hoda <asrafulhoda789@gmail.com>
2026-01-27 00:05:44 +0600

    for loop

---
+++ Module-15(Loops)/for_loop.js
@@ -0,0 +1,19 @@
+/**
+ * ---------------STRUCTURE-------------
+ * for( loop variable declaration;  condition;  increment){
+ * 
+ * }
+*/
+
+for(let num = 0; num <= 10; num++){
+    // console.log(num);
+}
+
+for(let i = 1; i <= 10; i++){
+    if(i%2 === 0){
+        console.log("Even: ", i);
+    }
+    else{
+        console.log("Odd: ", i);
+    }
+}
