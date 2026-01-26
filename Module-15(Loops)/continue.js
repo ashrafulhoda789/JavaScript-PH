ad7d20845106609487232a5b8fb031f83abbd969
ashraful-hoda <asrafulhoda789@gmail.com>
2026-01-27 00:22:58 +0600

    break and continue

---
+++ Module-15(Loops)/continue.js
@@ -0,0 +1,9 @@
+// continue --> skip this iteration.
+// break --> I am done with this loop. Loop end.
+
+for(let i=0; i<10; i++){
+    if(i%2 === 1){
+        continue;
+    }
+    console.log(i);
+}
