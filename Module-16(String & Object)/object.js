d6e8fc2aaae0761860ef43dfba3c03b235c11955
ashraful-hoda <asrafulhoda789@gmail.com>
2026-01-28 00:36:34 +0600

    object in javaScript

---
+++ Module-16(String & Object)/object.js
@@ -0,0 +1,30 @@
+// Primitive
+// Basic, primary data type
+const age = 21;
+const school = 'riffle square';
+const isPassed = true;
+let isDeveloper;
+
+const subjects = ['bangla', 'English', 'Physics', 'Math'];
+
+// object
+// It's non-primitive
+const bottle = {
+    brand: 'Apple',
+    price: 450,
+    color: 'White',
+    isClean: false ,
+
+};
+
+
+const subject = {
+    name: 'Biology',
+    teacher: 'Rejaul Karim',
+    examDate: '30 February',
+    chapters: ['First', 'Second', 'Third'],
+    exams: {
+        name: 'Mid Term',
+        marks: 100,
+    }
+};
