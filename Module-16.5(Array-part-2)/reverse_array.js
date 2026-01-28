36ef3322d709717bc5ce64d6c3d2e69069be0100
ashraful-hoda <asrafulhoda789@gmail.com>
2026-01-28 23:01:03 +0600

    reverse an array

---
+++ Module-16.5(Array-part-2)/reverse_array.js
@@ -0,0 +1,17 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7];
+
+// numbers.reverse();
+
+// console.log(numbers);
+
+const rev_numbers = [];
+for(const num of numbers){
+    rev_numbers.unshift(num);
+}
+
+// console.log(rev_numbers);
+
+for(let i = numbers.length - 1; i >= 0; i--){
+    console.log(numbers[i]);
+}
+
