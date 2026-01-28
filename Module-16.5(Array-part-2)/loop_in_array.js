345a1c72d63d8f21eafd1f2126870fd720c8fe91
ashraful-hoda <asrafulhoda789@gmail.com>
2026-01-28 22:51:09 +0600

    using loop in array

---
+++ Module-16.5(Array-part-2)/loop_in_array.js
@@ -0,0 +1,24 @@
+/**
+ * LOOPING TECHNIQUE
+ * for loop
+ * while loop
+ * do while
+ * for of
+ * for in --> Use in object
+*/
+
+const friends = ['Elon', 'Bill', 'Mark', 'Waren'];
+
+for(const friend of friends){
+    // console.log(friend);
+}
+
+for(let i = 0; i < friends.length; i++){
+    console.log(friends[i]);
+}
+
+const numbers = [5, 23, 456, 421, 25, 4512, 54541, 21, 45, 6];
+
+for(let i = 0; i < numbers.length; i++){
+    console.log(numbers[i]);
+}
