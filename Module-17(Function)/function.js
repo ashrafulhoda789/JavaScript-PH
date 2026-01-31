013b4fd1a9e9133a9f6137916ba6ad2e296e8bef
ashraful-hoda <asrafulhoda789@gmail.com>
2026-01-31 21:38:11 +0600

    parameter file created

--- Module-17(Function)/function.js
+++ Module-17(Function)/function.js
@@ -19,16 +19,3 @@ function brushTeeth() {
 
 // brushTeeth();
 
-function square(number){
-    return number*number;
-}
-
-const result = square(5);
-console.log(result);
-
-function add(num1, num2){
-    const sum = num1 + num2;
-    console.log(sum);
-}
-
-add(102, 6);
---
+++ Module-17(Function)/parameter.js
@@ -0,0 +1,13 @@
+function square(number){
+    return number*number;
+}
+
+const result = square(5);
+console.log(result);
+
+function add(num1, num2){
+    const sum = num1 + num2;
+    console.log(sum);
+}
+
+add(102, 6);
