08b8c44970a162028a1d805d30fab8e4487de257
ashraful-hoda <asrafulhoda789@gmail.com>
2026-01-31 20:57:14 +0600

    function declaration

---
+++ Module-17(Function)/function.js
@@ -0,0 +1,20 @@
+const number = 45;
+
+// Function declaration
+
+function fanOff(){
+    console.log('Click the switch to off of the Fan');
+}
+
+// call the function
+fanOff();
+
+// function name(){}
+
+function brushTeeth() {
+    console.log('Pick up the brush');
+    console.log('Add paste');
+    console.log('Now use it');    
+}
+
+brushTeeth();
