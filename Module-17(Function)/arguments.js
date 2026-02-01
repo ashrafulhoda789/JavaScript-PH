42ad1603d71115f30ff91d749b159d23871860bd
ashraful-hoda <asrafulhoda789@gmail.com>
2026-02-01 22:44:29 +0600

    arguments

---
+++ Module-17(Function)/arguments.js
@@ -0,0 +1,19 @@
+function doubleIt(number){
+    const doubled = number * 2;
+    console.log(number, doubled);
+}
+
+console.log("I will call the function");
+doubleIt(15);
+console.log("----------------------");
+doubleIt(88);
+
+
+function difference(num1, num2){
+    const diff = num1 - num2;
+    console.log(diff);
+}
+
+const wifeAge = 22;
+const myAge = 24;
+difference(myAge, wifeAge);
