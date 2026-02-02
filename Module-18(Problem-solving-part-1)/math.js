0e84fd988390a70083066e2aa4d1d929f11f258d
ashraful-hoda <asrafulhoda789@gmail.com>
2026-02-02 13:04:22 +0600

    Math object, abs, ceil, floor, round, min, max, random

---
+++ Module-18(Problem-solving-part-1)/math.js
@@ -0,0 +1,17 @@
+const min = Math.min(12, 19, 20, 8, 3);
+const max = Math.max(12, 19, 20, 8, 3);
+console.log(min);
+console.log(max);
+
+console.log(Math.PI);
+console.log(Math.abs(-34));
+
+console.log(Math.round(4.54));
+console.log(Math.ceil(2.455));
+console.log(Math.floor(2.455));
+
+console.log(Math.random());
+console.log(Math.random()*10);
+
+const rand = Math.round(Math.random()*10);
+console.log(rand);
