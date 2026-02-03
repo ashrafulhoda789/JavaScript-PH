4fc4851acf8b6db2e0a97a788b4a041463fac5c3
ashraful-hoda <asrafulhoda789@gmail.com>
2026-02-03 17:19:00 +0600

    we can call a function inside to a function

---
+++ Module-19(Problem-solving-part-2)/calculator.js
@@ -0,0 +1,43 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function division(num1, num2) {
+    return (num1 / num2).toFixed(2);
+}
+
+function remainder(num1, num2) {
+    return num1 % num2;
+}
+
+function calculator(a, b, operation) {
+    if(operation === 'add'){
+        return add(a,b);
+    }
+    else if(operation === 'subtract'){
+        return subtract(a,b);
+    }
+    else if(operation === 'multiply'){
+        return multiply(a,b);
+    }
+    else if(operation === 'division'){
+        return division(a,b);
+    }
+    else if(operation === 'remainder'){
+        return remainder(a,b);
+    }
+    else{
+        return "Only 'add', 'subtract', 'multiply', 'division' and 'remainder' operation is allowed."
+    }
+}
+
+const result = calculator(5, 7, 'division');
+console.log(result);
