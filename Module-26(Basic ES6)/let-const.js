e8f3d609c3997a4cd7d023a6dc9d621889d745bb
ashraful-hoda <asrafulhoda789@gmail.com>
2026-02-26 01:26:53 +0600

    let, const, var recap

---
+++ Module-26(Basic ES6)/let-const.js
@@ -0,0 +1,28 @@
+const name = 'Bangladesh';
+// name = 'Singapore';
+
+const countryName = 'BD_' + name;
+console.log(countryName);
+
+let price = 120;
+price = 150;
+
+console.log(price);
+
+// var not will be used
+
+const dress = ['shirt', 'pant', 'shoe'];
+dress.push('Belt');
+console.log(dress);
+
+
+const student = {
+    name : 'Jhone',
+    age : 24
+}
+
+// student = {
+//     name : '40'
+// }
+
+// console.log(student);
