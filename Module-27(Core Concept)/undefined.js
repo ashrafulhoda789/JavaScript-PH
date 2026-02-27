6cc5f5e5d09ccd9b59661fc361e540179025f4a1
ashraful-hoda <asrafulhoda789@gmail.com>
2026-02-27 19:15:10 +0600

    null and undefined

---
+++ Module-27(Core Concept)/undefined.js
@@ -0,0 +1,23 @@
+let data;
+console.log(data);
+
+const sum = (a, b) => {
+    console.log(a, b);
+};
+
+sum(5);
+
+const student = {
+    name : 'Jamshed',
+    age : 24,
+};
+
+delete student.name;
+// console.log(student.marks);
+console.log(student.name);
+
+const arr = [1,2,3,4];
+console.log(arr[5]);
+
+console.log(typeof undefined);
+console.log(typeof null);
