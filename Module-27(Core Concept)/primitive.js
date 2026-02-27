ca23e65d8b8dc06692b910d50b445c2886e52116
ashraful-hoda <asrafulhoda789@gmail.com>
2026-02-27 19:02:04 +0600

    primitive and non-primitive data types

---
+++ Module-27(Core Concept)/primitive.js
@@ -0,0 +1,20 @@
+let data = 4;
+data = 'Torok Makto';
+data = false;
+data = { price : 12};
+data = [
+
+]
+console.log(typeof data);
+
+// primitive data types
+let a = 4;
+let b = 'Torok makto';
+let c = false;
+
+// Non-primitive data types
+let arr = [];
+let object = {};
+function add(){
+    
+}
