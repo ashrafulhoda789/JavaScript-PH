379d1a4664ae80e3e8b0c837a5af41ee4cb5eac4
ashraful-hoda <asrafulhoda789@gmail.com>
2026-03-01 02:57:01 +0600

    filter, find, reduce

---
+++ Module-27(Core Concept)/filter.js
@@ -0,0 +1,9 @@
+const numbers = [1, 2, 3, 4, 5];
+
+const evenNumbers = numbers.filter(num => num % 2 === 0);
+
+console.log(evenNumbers);
+
+const friends = ['Jamshed', 'Mumir', 'Alo', 'Tomato', 'Jammir'];
+const JFriend = friends.filter(friend => friend[0] === 'J');
+console.log(JFriend);
---
+++ Module-27(Core Concept)/find.js
@@ -0,0 +1,4 @@
+const friends = ['Jamshed', 'Mumir', 'Alo', 'Tomato', 'Jammir'];
+
+const JFriend = friends.find(friend => friend[0] === 'J');
+console.log(JFriend);
